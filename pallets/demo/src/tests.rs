653ba038bec5bb5241808d9e80a22137d6fe6f7c
shyoski <ngonhuthanhtrung1409@gmail.com>
2022-07-25 15:17:53 +0700

    :white_check_mark: Add unit test for pallet demo

--- pallets/demo/src/tests.rs
+++ pallets/demo/src/tests.rs
@@ -4,7 +4,7 @@ use frame_support::{assert_noop, assert_ok};
 #[test]
 fn should_create_a_student_with_normal_name_and_age() {
 	new_test_ext().execute_with(|| {
-		assert_ok!(DemoModule::create_student(Origin::signed(1), b"shyoski", 24));
+		assert_ok!(DemoModule::create_student(Origin::signed(1), b"shyoski".to_vec(), 24));
 		assert_eq!(DemoModule::student_id(), 1);
 	});
 }
@@ -13,8 +13,8 @@ fn should_create_a_student_with_normal_name_and_age() {
 fn create_student_should_false_with_age_lower_than_20() {
 	new_test_ext().execute_with(|| {
 		assert_noop!(
-			DemoModule::create_student(Origin::signed(1), b"shyoski", 20),
-			Error::<T>::TooYoung
+			DemoModule::create_student(Origin::signed(1), b"shyoski".to_vec(), 20),
+			Error::<Test>::TooYoung
 		);
 	});
 }
